961ad515d1ef875583802a363143e096f684dc88
Dipa <dipashri2317kadam@gmail.com>
2021-07-19 13:36:52 +0530

    insertData

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const config = require("config");
 const app = express();
 require("./Initials/cors")(app);
 require("./Initials/db")();
+app.use(express.json({}));
+app.use(express.urlencoded({extended: true }));
 require("./Initials/routes")(app);
 
 
--- routes/prospectsets.js
+++ routes/prospectsets.js
@@ -13,5 +13,18 @@ router.get("/getProspectSet", async (req, res) => {
     return res.status(200).json({ status: 404, prospectSet: [] });
   });
 
+  router.post("/insertProspectSet", async (req, res) => {
+    let data = req.body
+     console.log(req.body)
+    let prospectSet = new ProspectSet(data);
+    prospectSet = await prospectSet.save()
+    if (prospectSet) {
+      return res
+        .status(200)
+        .json({ status: 200, message:"ProspectSet Added Successfully" });
+    }
+    return res.status(200).json({ status: 404, message:"Something went wrong" });
+  });
+
   
 module.exports = router;
